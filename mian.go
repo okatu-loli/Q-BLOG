1741c621d10831aa5c8be3f180829cc58bac403b
okatu-loli <CN_QianShi@hotmail.com>
2022-11-24 01:05:26 +0800

    导入gin框架，并验证

---
+++ mian.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	log.Fatal(r.Run())
+}
